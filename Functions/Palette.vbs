2164a4d467d04dfc322e1774fd1902c469895fe3
Philippe Majerus <ph.majerus@phm.lu>
2017-10-16 15:40:30 +0200

    Added stubs for palette.js and Palette.vbs modules

---
+++ Functions/Palette.vbs
@@ -0,0 +1,5 @@
+' This is a stub to load the Palette.vbs module.
+
+Option Explicit
+
+AXSH.LoadModule EnvVars.Expand("%UserProfile%\Documents\Majerus.net ActiveScript Shell\Modules\Palette.vbs"), "Palette"
---
+++ Functions/palette.js
@@ -0,0 +1,6 @@
+// Stub to load palette module.
+
+// Turn on strict mode for this file.
+"use strict";
+
+AXSH.loadModule(EnvVars.expand("%UserProfile%\\Documents\\Majerus.net ActiveScript Shell\\Modules\\palette.js"),"palette");
