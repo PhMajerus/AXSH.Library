145511b1687410e9a04255b166788325f6012a5e
Philippe Majerus <ph.majerus@phm.lu>
2017-11-27 15:15:27 +0100

    Not needed anymore in AXSH 2.0.10.0

--- Functions/palette.js
+++
@@ -1,6 +0,0 @@
-// Stub to load palette module.
-
-// Turn on strict mode for this file.
-"use strict";
-
-AXSH.loadModule(EnvVars.expand("%UserProfile%\\Documents\\Majerus.net ActiveScript Shell\\Modules\\palette.js"),"palette");
