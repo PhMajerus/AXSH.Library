73b5a64123b2238f19f62bd743db29e49462534f
Philippe Majerus <ph.majerus@phm.lu>
2018-06-08 23:43:53 +0200

    Fixes

--- Functions/saveFunc.js
+++ Functions/saveFunc.js
@@ -6,7 +6,6 @@
 ** - Philippe Majerus, June 2018
 */
 
-
 // Turn on strict mode for this file.
 "use strict";
 
@@ -22,8 +21,8 @@ function saveFunc(func) {
 		throw new Error("Function must include its name in its definition to be saved");
 	}
 	
-	var filePath = AXSH.envVars.expand("%UserProfile%\\Documents\\Majerus.net ActiveScript Shell\\Functions\\") + funcName + ".js";
 	var fso = new ActiveXObject("Scripting.FileSystemObject");
+	var filePath = fso.buildPath(AXSH.envVars.expand("%UserProfile%\\Documents\\Majerus.net ActiveScript Shell\\Functions"), funcName+".js");
 	var file = fso.createTextFile(filePath, true, false);
 	file.write("/*\r\n** Function "+ funcName +", saved using saveFunc,\r\n** on "+ new Date().toLocaleString() +".\r\n*/\r\n\r\n");
 	file.write(func+"\r\n");
