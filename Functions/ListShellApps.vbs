6dc687392b068ead537f66bc0671333fbeb0e9fd
Philippe Majerus <ph.majerus@phm.lu>
2019-02-01 18:52:05 +0100

    Updated to support name search
    Note this will require the WildcardsToRegExp functions available only in next build of ActiveScript Shell.

--- Functions/ListShellApps.vbs
+++ Functions/ListShellApps.vbs
@@ -7,12 +7,17 @@
 
 Option Explicit
 
-Sub ListShellApps
+Sub ListShellApps (NameSpecs)
+	Dim reNameSpecs
+	Set reNameSpecs = WildcardsToRegExp(NameSpecs)
+	
 	Dim ShellApp, FldrApps, Apps, Item
 	Set ShellApp = CreateObject("Shell.Application")
 	Set FldrApps = ShellApp.Namespace("shell:::{4234d49b-0245-4df3-b780-3893943456e1}") ' AppsFolder
 	Set Apps = FldrApps.Items
 	For Each Item In Apps
-		AXSH.Echo Chr(27)&"[93m"& Item.Name &Chr(27)&"[90m  shell:AppsFolder\"& Item.Path
+		If reNameSpecs.Test(Item.Name) Then
+			AXSH.Echo Chr(27)&"[93m"& Item.Name &Chr(27)&"[90m  shell:AppsFolder\"& Item.Path
+		End If
 	Next
 End Sub
--- Functions/listShellApps.js
+++ Functions/listShellApps.js
@@ -8,11 +8,18 @@
 */
 
 
-function listShellApps() {
+function listShellApps(nameSpecs) {
+	// If nameSpecs is provided and isn't already a RegExp, convert it to a RegExp
+	if ((nameSpecs !== undefined) && !(nameSpecs instanceof RegExp)) {
+		nameSpecs = wildcardsToRegExp(nameSpecs);
+	}
+	
 	var shellApp = new ActiveXObject("Shell.Application");
 	var fldrApps = shellApp.namespace("shell:::{4234d49b-0245-4df3-b780-3893943456e1}"); // AppsFolder
 	var apps = fldrApps.items();
 	new Enumerator(apps).forEach(function(item){
-		AXSH.echo("\x1B[93m"+ item.name +"\x1B[90m  shell:AppsFolder\\"+ item.path);
+		if (!nameSpecs || item.name.match(nameSpecs)) {
+			AXSH.echo("\x1B[93m"+ item.name +"\x1B[90m  shell:AppsFolder\\"+ item.path);
+		}
 	});
 }
