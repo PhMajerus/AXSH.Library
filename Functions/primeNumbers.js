3277c07e8651ce92adb0fa84d34aa25fef488fad
Philippe Majerus <ph.majerus@phm.lu>
2020-11-09 23:54:31 +0100

    Added async iterator to primeNumbers

--- Functions/primeNumbers.js
+++ Functions/primeNumbers.js
@@ -17,33 +17,60 @@
 ** A function is also available in AXSH to compensate for the lack of for...of
 ** iterate(primeNumbers, function(number){ echo(number) });
 ** 
-** - Philippe Majerus, November 2017.
+** The object also provides async iterator support:
+** for (var asyncIterator=primeNumbers["@@asyncIterator"](),item; !(item=await(asyncIterator.next())).done; ){ echo(item.value); };
+** 
+** A function is also available in AXSH to compensate for the lack of for await...of
+** asyncIterate(primeNumbers, function(number){ echo(number) });
+** 
+** - Philippe Majerus, November 2017, updated to add async in November 2020
 */
 
 
 // Prime numbers sequence object with "@@iterator" property.
-var primeNumbers = {
-	"@@iterator": function () {
-		return {
-			next: (function(){
-				function isPrime(number) {
-					var divisor = 2;
-					var numberSqrt = Math.sqrt(number);
-					while (divisor <= numberSqrt)
-						if (number % divisor++ == 0) return false;
-					return number > 1;
-				}
-				
-				return function () {
-					do {
-						this._current++
-					} while ((Number.MAX_SAFE_INTEGER > this._current) && !isPrime(this._current));
-					return (Number.MAX_SAFE_INTEGER > this._current)
-						? {value: this._current, done: false}
-						: {done: true};
-				}
-			}()),
-			_current: 1
-		};
+var primeNumbers = (function(){
+	
+	// Helper function to check if a number is a prime number
+	function isPrime(number) {
+		var divisor = 2;
+		var numberSqrt = Math.sqrt(number);
+		while (divisor <= numberSqrt)
+			if (number % divisor++ == 0) return false;
+		return number > 1;
 	}
-};
+	
+	return {
+		"@@iterator": function () {
+			return {
+				next: function () {
+						do {
+							this._current++
+						} while ((Number.MAX_SAFE_INTEGER > this._current) && !isPrime(this._current));
+						return (Number.MAX_SAFE_INTEGER > this._current)
+							? {value: this._current, done: false}
+							: {done: true};
+				},
+				_current: 1
+			};
+		},
+		
+		// Since calculating prime numbers can take a while, also provide an asynchronous iterator
+		"@@asyncIterator": function() {
+			return {
+				next: async(function(){
+					var _this = this;
+					return new Promise(function(resolve,reject) {
+						do {
+							_this._current++
+						} while ((Number.MAX_SAFE_INTEGER > _this._current) && !isPrime(_this._current));
+						resolve((Number.MAX_SAFE_INTEGER > _this._current)
+							? {value: _this._current, done: false}
+							: {done: true});
+					});
+				}),
+				_current: 1
+			};
+		}
+	};
+	
+})();
