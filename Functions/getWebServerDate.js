0cc7c0556eb791ad8369d25a7d9b6d38fdd9dc7b
Philippe Majerus <ph.majerus@phm.lu>
2022-08-08 02:33:03 +0200

    Added async version of getWebServerDate function

--- Functions/getWebServerDate.js
+++ Functions/getWebServerDate.js
@@ -5,6 +5,11 @@
 ** showDateTime(getWebServerDate("www.example.com"));
 ** // Checking if Web server and local time are out of sync by over 2 seconds.
 ** ?(Math.abs(new Date()-getWebServerDate("www.example.com")) > (2*1000))
+** 
+** This file also contains an async version that returns a promise:
+** getWebServerDate.async("www.example.com").then(function(dt){ showDateTime(dt); });
+** Note the async version does not allow urls with cross-domain redirects,
+** always use the final target url when using this version.
 */
 
 
@@ -30,6 +35,7 @@ function getWebServerDate(url) {
 	
 	// Perform request
 	whr.send();
+	
 	if (whr.status === 405) {
 		// Method not allowed - some HTTP servers refuse the HEAD request
 		// Simply perform a full GET request, not as efficient, but should
@@ -39,6 +45,7 @@ function getWebServerDate(url) {
 		whr.setRequestHeader("Accept-Charset", "utf-8, iso-8859-1;q=0.5");
 		whr.send();
 	}
+	
 	if (whr.status !== 200) {
 		throw new Error(whr.status.toString()+" "+whr.statusText);
 	}
@@ -56,3 +63,98 @@ function getWebServerDate(url) {
 	return new Date(Date.parse(date));
 }
 
+// Also provide an async version
+getWebServerDate.async = function (url) {
+	if (url.indexOf("://")==-1)
+		url = "http://" + url;
+	
+	// Use an XMLHttpRequest instead of a WinHttpRequest because WinHttpRequest events
+	// cannot be handled from scripts, and we need to handle the response asynchronously.
+	// 
+	// ServerXMLHTTP would be closer to WinHttpRequest than the client XMLHTTP version,
+	// but unfortunately it seems ServerXMLHTTP does not fire onreadystatechange or throw
+	// any error if the url cannot be resolved. We cannot have a promise that never settles.
+	// As a side effect of using the client library, the hostname handling is more strict,
+	// urls that require cross-domain redirects will fail. Always use the final target url.
+	var xhr;
+	try {
+		xhr = new ActiveXObject("MSXML2.XMLHTTP.6.0");
+	} catch(ex) {
+		if (ex.number === -2146827859) // 0x800A01AD "Automation server can't create object"
+			ex.description = "Microsoft Core XML Services (MSXML) 6.0 are not available";
+		throw ex;
+	}
+	
+	return new Promise(function(resolve, reject){
+		// Prepare an asynchronous HTTP HEAD request (using HEAD instead of GET to reduce data transfer)
+		try {
+			xhr.open("HEAD", url, true);
+		} catch(ex) {
+			if (ex.number === -2147012890) { // The URL does not use a recognized protocol
+				// When in async mode, this error does not get a description, so provide one.
+				reject(new Error("The URL does not use a recognized protocol"));
+			} else {
+				reject(ex);
+			}
+		}
+		xhr.setRequestHeader("User-Agent", "ActiveScript Shell getWebServerDate.js");
+		xhr.setRequestHeader("Accept-Charset", "utf-8, iso-8859-1;q=0.5");
+		
+		// Prepare handler
+		xhr.onreadystatechange = function() {
+			if (xhr.readyState === 4 /*COMPLETED*/) {
+				switch (xhr.status) {
+					case 405:
+						// Method not allowed - some HTTP servers refuse the HEAD request
+						// Simply perform a full GET request, not as efficient, but should
+						// always work.
+						// We don't need all the error handling again here as the URL has already been
+						// used successfully.
+						xhr.open("GET", url, true);
+						xhr.setRequestHeader("User-Agent", "ActiveScript Shell getWebServerDate.js");
+						xhr.setRequestHeader("Accept-Charset", "utf-8, iso-8859-1;q=0.5");
+						xhr.send();
+						// This handler will get called again as we reused the XHR object
+						// and left the onreadystatechange function as it.
+						break;
+						
+					case 200:
+						// Process header
+						var date;
+						try {
+							date = xhr.getResponseHeader("Date");
+						} catch(ex) {
+							if (ex.number === -2147012746) { // The requested header was not found
+								reject(new Error("The web server did not provide its current time"));
+							} else {
+								reject(ex);
+							}
+						}
+						resolve(new Date(Date.parse(date)));
+						break;
+						
+					case 12007:
+						// When in async mode, this error does not get a description in the default
+						// handler, so we process it manually.
+						reject(new Error("The server name or address could not be resolved"));
+						break;
+						
+					default:
+						reject(new Error(xhr.status.toString()+" "+xhr.statusText));
+				}
+			}
+		};
+		
+		// Perform request
+		try {
+			xhr.send();
+		} catch(ex) {
+			// We can fail here if the URL syntax ends up being invalid.
+			if (ex.number === -2147221020) { // Invalid syntax
+				reject(new Error("The URL syntax is invalid"));
+			} else {
+				reject(ex);
+			}
+		}
+	});
+};
