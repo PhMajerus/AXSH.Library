1844a05fef6fe82c17f391154f54a04adbf928c0
Philippe Majerus <ph.majerus@phm.lu>
2022-10-30 23:47:25 +0100

    Added example usage

--- Functions/EGAColor.vbs
+++ Functions/EGAColor.vbs
@@ -10,6 +10,11 @@
 ' Note the CGA (IBM Color Graphics Adapter) colors are not completely
 ' sequential because RGBI monitors replaced dark yellow with brown.
 ' The CGA palette are indexes 0,1,2,3,4,5,20,7,56,57,58,59,60,61,62,63.
+' 
+' Example usage, show 4x4x4 colors cube:
+' Set ANSI = CreateObject("Majerus.ANSI")
+' Function EC(R,G,B): EC=(R And 1)*32+(G And 1)*16+(B And 1)*8+(R And 2)*2+(G And 2)+(B And 2)\2: End Function
+' For R=0 To 3: S="": For G=0 To 3: For B=0 To 3: I=EC(R,G,B): S=S+ANSI.BackColor(EGAColor(I))&" "&Right("00"&I,2)&" ": Next: S=S+Chr(27)&"[m  ": Next: Echo S: Next
 '
 
 Option Explicit
--- Functions/egaColor.js
+++ Functions/egaColor.js
@@ -10,6 +10,12 @@
 ** Note the CGA (IBM Color Graphics Adapter) colors are not completely
 ** sequential because RGBI monitors replaced dark yellow with brown.
 ** The CGA palette are indexes 0,1,2,3,4,5,20,7,56,57,58,59,60,61,62,63.
+** 
+** Example usage, show 4x4x4 colors cube:
+** var ansi=new ActiveXObject("Majerus.ANSI");
+** function ec(r,g,b){ return ((r&1)<<5)+((g&1)<<4)+((b&1)<<3)+((r&2)<<1)+(g&2)+((b&2)>>1); }
+** for(var r=0;r<4;r++){var s="";for(var g=0;g<4;g++){for(var b=0;b<4;b++){var i=ec(r,g,b);s+=ansi.backColor(egaColor(i))+" "+("00"+i.toString()).slice(-2)+" "; };s+="\x1B[m  "};echo(s);};
+**
 */
 
 
